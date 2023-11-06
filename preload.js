bdf7801a2162518541aed367ca97795eca3acae7
Luisb770 <43124829+Luisb770@users.noreply.github.com>
2023-11-05 16:33:18 -0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,8 @@
 //This file is the preload script. It exposes the Node.js API to the renderer process.
+//The code uses the contextBridge module to expose a specific set of Node.js APIs to the renderer process. 
+//This is a more secure approach, as it allows you to restrict the permissions of the renderer process.
+//For example, you could use the contextBridge module to expose only the ipcRenderer module to the renderer process. 
+//This would prevent the renderer process from accessing any other Node.js modules.
 
 const { contextBridge } = require('electron');
 
